a1138d515a23cf80521bbc88b78f1c0627a4143c
Breno Mascarenhas <brenoliveira8@gmail.com>
2023-05-08 15:02:21 -0300

    :sparkles: Exercise 10

---
+++ src/ex10/Solution.java
@@ -0,0 +1,21 @@
+package ex10;
+
+import java.util.Scanner;
+
+public class Solution{
+    static {
+        Scanner scanner = new Scanner(System.in);
+
+        int b = scanner.nextInt();
+        int h = scanner.nextInt();
+
+        if((b > 0) && (h > 0)){
+            System.out.println(b*h);
+        }else {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args){
+    }
+}
