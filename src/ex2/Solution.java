2ac3483e264a64a4282552669883ee93dc12cc9a
Breno Mascarenhas <brenoliveira8@gmail.com>
2023-05-08 12:41:18 -0300

    :tada: First commit

---
+++ src/ex2/Solution.java
@@ -0,0 +1,17 @@
+package ex2;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+    }
+}
---
+++ src/ex5/Solution.java
@@ -0,0 +1,75 @@
+package ex5;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.next();
+        int i1 = scanner.nextInt();
+        String s2 = scanner.next();
+        int i2 = scanner.nextInt();
+        String s3 = scanner.next();
+        int i3 = scanner.nextInt();
+
+        int size1 = s1.length();
+        int size2 = s2.length();
+        int size3 = s3.length();
+
+        while(s1.length() != 15){
+            s1 += " ";
+        }
+        if(i1 >= 100){
+            s1 += i1;
+        }
+        if(i1 < 100 && i1 > 0){
+            s1 += "0"+i1;
+        }
+        if(i1 < 10 && i1 > 0){
+            s1 += "00"+i1;
+        }
+        if(i1 == 0){
+            s1 += "000";
+        }
+
+        while(s2.length() != 15){
+            s2 += " ";
+        }
+        if(i2 >= 100){
+            s2 += i2;
+        }
+        if(i2 < 100 && i2 > 0){
+            s2 += "0"+i2;
+        }
+        if(i2 < 10 && i2 > 0){
+            s2 += "00"+i2;
+        }
+        if(i2 == 0){
+            s2 += "000";
+        }
+
+        while(s3.length() != 15){
+            s3 += " ";
+        }
+        if(i3 >= 100){
+            s3 += i3;
+        }
+        if(i3 < 100 && i3 > 0){
+            s3 += "0"+i3;
+        }
+        if(i3 < 10 && i3 > 0){
+            s3 += "00"+i3;
+        }
+        if(i3 == 0){
+            s3 += "000";
+        }
+
+        System.out.println("================================");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println("================================");
+    }
+}
