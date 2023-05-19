c0224024c67fc97e71d11c71705771308aff0a49
Breno Mascarenhas <brenoliveira8@gmail.com>
2023-05-18 21:13:39 -0300

    :sparkles: Exercise 27 (Easy)

---
+++ src/ex21_30/ex27/Solution.java
@@ -0,0 +1,25 @@
+package ex21_30.ex27;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Solution{
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+
+        BigInteger number = new BigInteger(n);
+
+        boolean probablePrime = number.isProbablePrime(10);
+
+        if(probablePrime)
+            System.out.println("prime");
+        else
+            System.out.println("not prime");
+
+        bufferedReader.close();
+    }
+}
