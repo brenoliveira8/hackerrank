3e59f981f72fe060adc25ebc4fe4150a02b917de
Breno Mascarenhas <brenoliveira8@gmail.com>
2023-05-31 14:09:00 -0300

    :sparkles: Exercise 38 (Easy) - solved

--- src/javaExercises/ex31_40/ex38/Solution.java
+++ src/javaExercises/ex31_40/ex38/Solution.java
@@ -1,4 +1,29 @@
 package javaExercises.ex31_40.ex38;
 
-public class Solution {
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+
+class Bird extends Animal{
+    void fly(){
+        System.out.println("I am flying");
+    }
+
+    public void sing(){
+        System.out.println("I am singing");
+    }
+}
+
+public class Solution{
+
+    public static void main(String args[]){
+
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+
+    }
 }
